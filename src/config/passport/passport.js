87c46ca19293d36589186c475cef7a63611acc0d
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-04-15 05:11:52 -0500

    Commit No. 3: Implementación de JWT Strategy en el proyecto - Clase 22

--- src/config/passport/passport.js
+++ src/config/passport/passport.js
@@ -4,6 +4,7 @@ import GithubStrategy from 'passport-github2'
 import crypto from 'crypto'
 import { userModel } from '../../models/user.js'
 import { createHash, validatePassword } from '../../utils/bcrypt.js'
+import { strategyJWT } from './strategies/jwtStrategy.js'
 
 const localStrategy = local.Strategy
 
@@ -46,27 +47,31 @@ const initializePassport = () => {
         }
     }))
 
-    passport.use('github', new GithubStrategy({
-        clientID: "TU_CLIENT_ID",
-        clientSecret: "TU_CLIENT_SECRET",
-        callbackURL: "http://localhost:11000/api/session/github"
-    }, async (accessToken, refreshToken, profile, done) => {
-        try{
-            const user = await userModel.findOne({ email: profile._json.email}).lean()
-            if (user){
-                done(null, user)
-            } else {
-                const randomNumber = crypto.randomUUID()
-                console.log(profile._json)
-                const userCreated = await userModel.create({ first_name: profile._json.name, last_name: ' ', email: profile._json.email, age: 18, password: createHash(`${profile._json.name}`) })
-                console.log(randomNumber)
-                return done(null, userCreated)
-            }
-        } catch (error){
-            return done(error)
-        }
-    }))
+    // passport.use('github', new GithubStrategy({
+    //     clientID: "TU_CLIENT_ID",
+    //     clientSecret: "TU_CLIENT_SECRET",
+    //     callbackURL: "http://localhost:11000/api/session/github"
+    // }, async (accessToken, refreshToken, profile, done) => {
+    //     try{
+    //         const user = await userModel.findOne({ email: profile._json.email}).lean()
+    //         if (user){
+    //             done(null, user)
+    //         } else {
+    //             const randomNumber = crypto.randomUUID()
+    //             console.log(profile._json)
+    //             const userCreated = await userModel.create({ first_name: profile._json.name, last_name: ' ', email: profile._json.email, age: 18, password: createHash(`${profile._json.name}`) })
+    //             console.log(randomNumber)
+    //             return done(null, userCreated)
+    //         }
+    //     } catch (error){
+    //         return done(error)
+    //     }
+    // }))
+
+passport.use('jwt', strategyJWT)
 
 }
+
+
 
 export default initializePassport
